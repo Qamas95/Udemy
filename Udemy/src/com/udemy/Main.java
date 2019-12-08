1d47faf261ba14eed67b76117e99a6c0ea4a1b63
Qamas95 <kamil.p.kubiak@gmail.com>
2019-12-08 17:52:05 +0100

    Task: Write a class with the name Wall. The class needs two fields (instance variables) with name width and height  of type double.
    The class needs to have two constructors. The first constructor does not have any parameters (no-arg constructor).
    The second constructor has parameters width and height of type double and it needs to initialize the fields.
    In case the width is less than 0 it needs to set the width field value to 0, in case the height parameter
     is less than 0 it needs to set the height field value to 0.
    
    Write the following methods (instance methods):
    * Method named getWidth without any parameters, it needs to return the value of width field.
    * Method named getHeight without any parameters, it needs to return the value of height field.
    * Method named setWidth with one parameter of type double, it needs to set the value of the width field.
    If the parameter is less than 0 it needs to set the width field value to 0.
    * Method named setHeight with one parameter of type double, it needs to set the value of the height field.
    If the parameter is less than 0 it needs to set the height field value to 0.
    * Method named getArea without any parameters, it needs to return the area of the wall.
    
    TEST EXAMPLE
    
    → TEST CODE:
    
    1 Wall wall = new Wall(5,4);
    2 System.out.println("area= " + wall.getArea());
    3
    4 wall.setHeight(-1.5);
    5 System.out.println("width= " + wall.getWidth());
    6 System.out.println("height= " + wall.getHeight());
    7 System.out.println("area= " + wall.getArea());
    
    → OUTPUT:
    
    area= 20.0
    width= 5.0
    height= 0.0
    area= 0.0
    
    NOTE: All methods should be defined as public NOT public static.
    
    NOTE: In total, you have to write 5 methods and 2 constructors.
    
    NOTE: Do not add a main method to the solution code.

--- Udemy/src/com/udemy/Main.java
+++ Udemy/src/com/udemy/Main.java
@@ -4,44 +4,12 @@ package com.udemy;
 public class Main {
     public static void main(String[] args) {
 
-/*        System.out.println("**************");
-        Company test3 = new Company();
-
-        System.out.println(test3.getEmployee_name());
-
-        Company test2 = new Company("Qamas2","Kubiak2",24,4000,"@gmail.com");
-
-        test2.withdrawFromBalance(200);
-        System.out.println(test2.getEmployee_age());
-        System.out.println(test2.getEmployee_account_cash_balance());
-        System.out.println("**************");
-        Company test = new Company();
-
-        test.setEmployee_email("Qamas");
-        test.setEmployee_surname("Kubiak");
-        test.setEmployee_age(24);
-        test.setEmployee_account_cash_balance(3000);
-        test.setEmployee_email("wp@pl");
-
-        test.addToBalance(400);
-        test.withdrawFromBalance(3200);
-
-
-        System.out.println("**************");
-        Company test4 = new Company("Qamas3","Kubiak3","@op.pl");
-
-        test4.withdrawFromBalance(5000);*/
-
-        VipCustomer customer1 = new VipCustomer();
-        System.out.println(customer1.getName());
-
-        VipCustomer customer2 = new VipCustomer("Qamas", 25000.25);
-        System.out.println(customer2.getName());
-
-        VipCustomer customer3 = new VipCustomer("Bob",60000.00,"tim@gmail.com");
-
-        System.out.println(customer3.getName());
-        System.out.println(customer3.getEmail());
+        Wall wall = new Wall(1.125,-1.25);
+        System.out.println("area= " + wall.getArea());
+       // wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
 
     }
 
