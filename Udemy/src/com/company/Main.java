b53f4557a67e134dd77f0a16abd7b23eece3221d
Qamas95 <kamil.p.kubiak@gmail.com>
2019-12-11 23:05:21 +0100

    Inheritance examples...

---
+++ Udemy/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Animal animal = new Animal("Animal",1,1,5,5);
+
+        Dog dog = new Dog("Boxer",12,32,2,4,1,32,"Brown");
+      //  dog.eat();
+        dog.walk();
+        //dog.run();
+
+    }
+
+}
