e48584de1b2b9ee215c2589dce14cf8dfa5ab7b0
Qamas95 <kamil.p.kubiak@gmail.com>
2019-12-14 22:19:03 +0100

    Composition task:
            // Create a single room of a house using composition.
            // Think about the things that should be included in the room.
            // Maybe physical parts of the house but furniture as well
            // Add at least one method to access an object via a getter and
            // then that objects public method as you saw in the previous video
            // then add at least one method to hide the object e.g. not using a getter
            // but to access the object used in composition within the main class
            // like you saw in this video.

--- Udemy/src/com/udemy/Main.java
+++ Udemy/src/com/udemy/Main.java
@@ -4,13 +4,22 @@ package com.udemy;
 public class Main {
     public static void main(String[] args) {
 
-        Dimensions dimensions = new Dimensions(20, 20, 5);
-        Case theCase = new Case("220B", "Dell", "240", dimensions);
-        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(240, 1440));
-        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
+        Wall wall1 = new Wall("East");
+        Wall wall2 = new Wall("West");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("North");
 
-        PC thePC = new PC(theCase,theMonitor,theMotherboard);
-        thePC.powerUp();
+        Ceiling ceiling = new Ceiling(12, 55);
+
+        Bed bed = new Bed("Modern", 4, 3, 2, 1);
+
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedroom = new Bedroom("Qamas Room", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+
+
+        bedroom.makeBed();
+        bedroom.getLamp().turnOn();
 
     }
 
--- Udemy/src/com/udemy/Wall.java
+++ Udemy/src/com/udemy/Wall.java
@@ -2,52 +2,13 @@ package com.udemy;
 
 public class Wall {
 
-    private double width;
-    private double height;
+    private String direction;
 
-
-    public Wall(){
-
+    public Wall(String direction) {
+        this.direction = direction;
     }
 
-    public Wall(double width, double height) {
-
-        if(width < 0 && height < 0) {
-            this.width = 0;
-            this.height = 0;
-        } else if(width < 0) {
-            this.width = 0;
-            this.height = height;
-        } else if (height < 0) {
-            this.height = 0;
-            this.width = width;
-        } else {
-            this.width = width;
-            this.height = height;
-        }
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        if(width < 0){
-            this.width = 0;
-        } else this.width = width;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        if(height < 0){
-            this.height = 0;
-        } else this.height = height;
-    }
-
-    public double getArea(){
-        return height*width;
+    public String getDirection() {
+        return direction;
     }
 }
