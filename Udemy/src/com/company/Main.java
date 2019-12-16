2c03f807004f70265a2365d57ded69a14320090c
Qamas95 <kamil.p.kubiak@gmail.com>
2019-12-16 21:11:41 +0100

    Task: 	    // We are going to go back to the car analogy.         // Create a base class called Car         // It should have a few fields that would be appropriate for a generice  car calss.         // engine, cylinders, wheels, etc.         // Constructor should initialize cylinders (number of) and name, and set wheels to 4         // and engine to true. Cylinders and names would be passed parameters.         //         // Create appropriate getters         //         // Create some methods like startEngine, accelerate, and brake         //         // show a message for each in the base class         // Now create 3 sub classes for your favorite vehicles.         // Override the appropriate methods to demonstrate polymorphism in use.         // put all classes in the one java file (this one).

--- Udemy/src/com/company/Main.java
+++ Udemy/src/com/company/Main.java
@@ -1,70 +1,101 @@
 package com.company;
 
-class Movie {
+class Car {
+    private boolean engine;
     private String name;
+    private int cylinders;
+    private int wheels;
 
-    public Movie(String name) {
+    public Car(String name, int cylinders) {
         this.name = name;
-    }
-
-    public String plot() {
-        return "No plot here";
+        this.cylinders = cylinders;
+        this.wheels = 4;
+        this.engine = true;
     }
 
     public String getName() {
         return name;
     }
-}
 
-class Jaws extends Movie {
-    public Jaws() {
-        super("Jaws");
+    public int getCylinders() {
+        return cylinders;
     }
 
-    public String plot() {
-        return "A shark eats lots of people";
+    public String startEngine(){
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate(){
+        return "Car -> accelerate()";
+    }
+
+    public String brake() {
+        return "Car -> brake()";
     }
 }
 
-class IndependenceDay extends Movie {
-    public IndependenceDay() {
-        super("Independece Day");
+
+class Mitsubishi extends Car {
+    public Mitsubishi(String name, int cylinders) {
+        super(name, cylinders);
     }
 
     @Override
-    public String plot() {
-        return "Aliens attempt to take over panet earth";
-    }
-}
-
-class MazeRunner extends Movie {
-    public MazeRunner() {
-        super("Maze Runner");
+    public String startEngine() {
+        return "Mitsubishi -> startEngine()";
     }
 
     @Override
-    public String plot() {
-        return "Kids try and escape a maze";
-    }
-}
-
-class StarWars extends Movie {
-    public StarWars() {
-        super("Star Wars");
+    public String accelerate() {
+        return "Mitsubishi -> accelerate()";
     }
 
     @Override
-    public String plot() {
-        return "Imperial Forces try to take over the universe";
+    public String brake() {
+        return "Mitsubishi -> brake()";
     }
 }
 
-class Forgetable extends Movie {
-    public Forgetable() {
-        super("Forgetable");
+class Holden extends Car {
+    public Holden(String name, int cylinders) {
+        super(name, cylinders);
     }
 
-    // No plot method
+    @Override
+    public String startEngine() {
+        return getClass().getSimpleName() + " -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() + " -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return getClass().getSimpleName() + " -> brake()";
+    }
+}
+
+class Ford extends Car {
+    public Ford(String name, int cylinders) {
+        super(name, cylinders);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Ford -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Ford -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Ford -> brake()";
+    }
 }
 
 
@@ -72,37 +103,30 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Car car = new Car("Base car",8);
 
-        for (int i = 1; i < 11; i++) {
-            Movie movie = randomMovie();
-            System.out.println("Movie #" + i + " : " + movie.getName() + "\n" +
-                    "Plot: " + movie.plot() +
-                    "\n");
-        }
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
 
+
+        Mitsubishi mitsubishi = new Mitsubishi("Outlander VRW 4WD", 6);
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+
+        Ford ford = new Ford("Ford Falcon", 6);
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
+        Holden holden = new Holden("Ford Falcon", 6);
+        System.out.println(holden.startEngine());
+        System.out.println(holden.accelerate());
+        System.out.println(holden.brake());
     }
 
-
-    public static Movie randomMovie() {
-        int randomNumber = (int) (Math.random() * 5) + 1;
-
-        System.out.println("Random number generated Was " + randomNumber);
-
-        switch (randomNumber) {
-            case 1:
-                return new Jaws();
-            case 2:
-                return new IndependenceDay();
-            case 3:
-                return new MazeRunner();
-            case 4:
-                return new StarWars();
-            case 5:
-                return new Forgetable();
-            default:
-                return null;
-        }
-        // return null;
-    }
 
 }
+
