d74d3c4cff1c5c586ebde068e44d12b7eb9c9f33
Qamas95 <kamil.p.kubiak@gmail.com>
2019-12-16 20:03:55 +0100

    Polymorphism example

--- Udemy/src/com/company/Main.java
+++ Udemy/src/com/company/Main.java
@@ -1,28 +1,108 @@
 package com.company;
 
+class Movie {
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String plot() {
+        return "No plot here";
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Jaws extends Movie {
+    public Jaws() {
+        super("Jaws");
+    }
+
+    public String plot() {
+        return "A shark eats lots of people";
+    }
+}
+
+class IndependenceDay extends Movie {
+    public IndependenceDay() {
+        super("Independece Day");
+    }
+
+    @Override
+    public String plot() {
+        return "Aliens attempt to take over panet earth";
+    }
+}
+
+class MazeRunner extends Movie {
+    public MazeRunner() {
+        super("Maze Runner");
+    }
+
+    @Override
+    public String plot() {
+        return "Kids try and escape a maze";
+    }
+}
+
+class StarWars extends Movie {
+    public StarWars() {
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "Imperial Forces try to take over the universe";
+    }
+}
+
+class Forgetable extends Movie {
+    public Forgetable() {
+        super("Forgetable");
+    }
+
+    // No plot method
+}
+
+
 public class Main {
 
     public static void main(String[] args) {
 
 
-        House blueHouse = new House("blue");
-        House anotherHouse = blueHouse;
+        for (int i = 1; i < 11; i++) {
+            Movie movie = randomMovie();
+            System.out.println("Movie #" + i + " : " + movie.getName() + "\n" +
+                    "Plot: " + movie.plot() +
+                    "\n");
+        }
 
-        System.out.println(blueHouse.getColor());
-        System.out.println(anotherHouse.getColor());
-
-        anotherHouse.setColor("red");
-        System.out.println(blueHouse.getColor());
-        System.out.println(anotherHouse.getColor());
-
-        House greenHouse = new House("green");
-        anotherHouse = greenHouse;
-
-        System.out.println(blueHouse.getColor());
-        System.out.println(anotherHouse.getColor());
-        System.out.println(greenHouse.getColor());
+    }
 
 
+    public static Movie randomMovie() {
+        int randomNumber = (int) (Math.random() * 5) + 1;
+
+        System.out.println("Random number generated Was " + randomNumber);
+
+        switch (randomNumber) {
+            case 1:
+                return new Jaws();
+            case 2:
+                return new IndependenceDay();
+            case 3:
+                return new MazeRunner();
+            case 4:
+                return new StarWars();
+            case 5:
+                return new Forgetable();
+            default:
+                return null;
+        }
+        // return null;
     }
 
 }
