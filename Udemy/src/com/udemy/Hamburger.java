dbb3907309da83546ca4f5df093256a869437132
Qamas95 <kamil.p.kubiak@gmail.com>
2019-12-21 23:28:57 +0100

    See changes

--- Udemy/src/com/udemy/Hamburger.java
+++ Udemy/src/com/udemy/Hamburger.java
@@ -1,7 +1,6 @@
 package com.udemy;
 
 public class Hamburger {
-
     private String name;
     private String meat;
     private double price;
@@ -26,7 +25,6 @@ public class Hamburger {
         this.breadRollType = breadRollType;
     }
 
-
     public void addHamburgerAddition1(String name, double price) {
         this.addition1Name = name;
         this.addition1Price = price;
@@ -36,12 +34,10 @@ public class Hamburger {
         this.addition2Name = name;
         this.addition2Price = price;
     }
-
     public void addHamburgerAddition3(String name, double price) {
         this.addition3Name = name;
         this.addition3Price = price;
     }
-
     public void addHamburgerAddition4(String name, double price) {
         this.addition4Name = name;
         this.addition4Price = price;
@@ -49,24 +45,40 @@ public class Hamburger {
 
     public double itemizeHamburger() {
         double hamburgerPrice = this.price;
-        System.out.println(this.name + " hamburger " + "on a " +
-                this.breadRollType + " roll " + "with " + this.meat + " price is " + this.price);
-        if (this.addition1Name != null) {
+        System.out.println(this.name + " hamburger " + " on a " + this.breadRollType + " roll "
+                + "with " + this.meat + ", price is "  + this.price);
+        if(this.addition1Name != null) {
             hamburgerPrice += this.addition1Price;
             System.out.println("Added " + this.addition1Name + " for an extra " + this.addition1Price);
         }
-        if (this.addition2Name != null) {
+        if(this.addition2Name != null) {
             hamburgerPrice += this.addition2Price;
             System.out.println("Added " + this.addition2Name + " for an extra " + this.addition2Price);
         }
-        if (this.addition3Name != null) {
+        if(this.addition3Name != null) {
             hamburgerPrice += this.addition3Price;
             System.out.println("Added " + this.addition3Name + " for an extra " + this.addition3Price);
         }
-        if (this.addition4Name != null) {
+        if(this.addition4Name != null) {
             hamburgerPrice += this.addition4Price;
             System.out.println("Added " + this.addition4Name + " for an extra " + this.addition4Price);
         }
+
         return hamburgerPrice;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- Udemy/src/com/udemy/Main.java
+++ Udemy/src/com/udemy/Main.java
@@ -5,19 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Hamburger hamburger = new Hamburger("Bassic","Sausage",3.65,"White");
-        //double price = hamburger.itemizeHamburger();
-        hamburger.addHamburgerAddition1("Tomato",0.27);
-        hamburger.addHamburgerAddition2("Lettuce",0.75);
-        hamburger.addHamburgerAddition3("Cucumber",0.5);
-        hamburger.addHamburgerAddition4("Cheese",1.12);
-        System.out.println("Total burger price is " + hamburger.itemizeHamburger());
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+        System.out.println("Total Burger price is " + hamburger.itemizeHamburger());
 
-        HealthlyBurger healthlyBurger = new HealthlyBurger("Bacon",5.67);
-        healthlyBurger.itemizeHamburger();
-        healthlyBurger.addHealthAddition1("Egg",3);
-        healthlyBurger.itemizeHamburger();
-
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.itemizeHamburger();
     }
 
 }
