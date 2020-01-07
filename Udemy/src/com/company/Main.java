01fbcee142e71dfc853733ac2b95b3762daf0c5c
Qamas95 <kamil.p.kubiak@gmail.com>
2020-01-07 22:40:48 +0100

    Task: -Write a method called reverse() with an int array as a parameter.
    -The method should not return any value. In other words, the method
     is allowed to modify the array parameter.
    
    -In main() test the reverse() method and print the array both
    reversed and non-reversed.
    
    -To reverse the array, you have to swap the elements, so
    that the first element is swapped with the last element and so on.
    
    -For example, if the array is {1, 2, 3, 4, 5}, then the
     reversed array is {5, 4, 3, 2, 1}.
    
    Tip:
    	-Create a new console project with the name eReverseArrayChallengef

--- Udemy/src/com/company/Main.java
+++ Udemy/src/com/company/Main.java
@@ -2,47 +2,31 @@ package com.company;
 
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-        System.out.println("Enter count:");
-        int count = scanner.nextInt();
-        scanner.nextLine();
-
-        int[] returnedArray = readIntegers(count);
-
-        int returnedMin = findMin(returnedArray);
-
-        System.out.println("min = " + returnedMin);
+        int[] array = {1, 5, 3, 7, 11, 9, 15};
+        System.out.println("Array = " + Arrays.toString(array));
+        reverse(array);
+        System.out.println("Reversed array = " + Arrays.toString(array));
 
     }
 
-    private static int[] readIntegers(int count) {
-        int[] array = new int[count];
-        for (int i = 0; i < array.length; i++) {
-            System.out.println("Enter a number:");
-            int number = scanner.nextInt();
-            scanner.nextLine();
-            array[i] = number;
-        }
-        return array;
-    }
 
-    private static int findMin(int[] array){
-        int min = Integer.MAX_VALUE;
+    public static void reverse(int[] array) {
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
 
-        for (int i=0; i<array.length; i++){
-            int value = array[i];
-            if(value < min) {
-                min = value;
-            }
+        for (int i = 0; i < halfLength; i++) {
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
         }
-        return min;
+
     }
 
 }
