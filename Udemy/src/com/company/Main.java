439fb0c6b84409a96315b37762b8215ad6ed95e8
Qamas95 <kamil.p.kubiak@gmail.com>
2020-01-09 22:34:50 +0100

    Array List 3/3

--- Udemy/src/com/company/Main.java
+++ Udemy/src/com/company/Main.java
@@ -1,25 +1,26 @@
 package com.company;
 
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-private static Scanner scanner = new Scanner(System.in);
-private static GroceryList groceryList = new GroceryList();
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static GroceryList groceryList = new GroceryList();
 
     public static void main(String[] args) {
-
         boolean quit = false;
         int choice = 0;
-        printInstruction();
-        while (!quit){
-            System.out.println("Enter ur choice :");
+
+        printInstructions();
+        while (!quit) {
+            System.out.println("Enter your choice: ");
             choice = scanner.nextInt();
             scanner.nextLine();
 
-            switch (choice){
+            switch (choice) {
                 case 0:
-                    printInstruction();
+                    printInstructions();
                     break;
                 case 1:
                     groceryList.printGroceryList();
@@ -28,61 +29,73 @@ private static GroceryList groceryList = new GroceryList();
                     addItem();
                     break;
                 case 3:
-                    modyfyItem();
+                    modifyItem();
                     break;
                 case 4:
                     removeItem();
                     break;
                 case 5:
-                    searchforItem();
+                    searchForItem();
                     break;
                 case 6:
+                    processArrayList();
+                case 7:
                     quit = true;
                     break;
             }
         }
     }
 
-    public static void printInstruction(){
+    public static void printInstructions() {
         System.out.println("\nPress ");
-        System.out.println("\t 0 - To print choice options");
-        System.out.println("\t 1 - To print the list of grocery items");
+        System.out.println("\t 0 - To print choice options.");
+        System.out.println("\t 1 - To print the list of grocery items.");
         System.out.println("\t 2 - To add an item to the list.");
-        System.out.println("\t 3 - To modify an item in the list");
-        System.out.println("\t 4 - To remove an item from the list");
-        System.out.println("\t 5 - To search for an item in the list");
-        System.out.println("\t 6 - To quit the application");
+        System.out.println("\t 3 - To modify an item in the list.");
+        System.out.println("\t 4 - To remove an item from the list.");
+        System.out.println("\t 5 - To search for an item in the list.");
+        System.out.println("\t 6 - To quit the application.");
     }
 
-    public static void addItem(){
-        System.out.println("Please enter the grocery item");
+    public static void addItem() {
+        System.out.print("Please enter the grocery item: ");
         groceryList.addGroceryItem(scanner.nextLine());
     }
 
-    public static void modyfyItem(){
-        System.out.println("Enter item number");
-        int itemNo = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Enter replecement item: ");
+    public static void modifyItem() {
+        System.out.print("Current item name: ");
+        String itemNo = scanner.nextLine();
+        System.out.print("Enter new item: ");
         String newItem = scanner.nextLine();
-        groceryList.modifyGroceryItem(itemNo-1, newItem);
+        groceryList.modifyGroceryItem(itemNo, newItem);
     }
 
-    public static void removeItem(){
-        System.out.println("Enter item number");
-        int itemNo = scanner.nextInt();
-        scanner.nextLine();
-        groceryList.removeGroceryItem(itemNo-1);
+    public static void removeItem() {
+        System.out.print("Enter item name: ");
+        String itemNo = scanner.nextLine();
+        groceryList.removeGroceryItem(itemNo);
     }
 
-    public static void searchforItem(){
-        System.out.println("Item to search for: ");
+    public static void searchForItem() {
+        System.out.print("Item to search for: ");
         String searchItem = scanner.nextLine();
-        if(groceryList.findItem(searchItem) != null){
-            System.out.println("Found " + searchItem + " in our grocery list");
+        if (groceryList.onFile(searchItem)) {
+            System.out.println("Found " + searchItem);
         } else {
-            System.out.println(searchItem + " is not in the shopping list");
+            System.out.println(searchItem + ", not on file.");
         }
     }
-}
 
+    public static void processArrayList() {
+        ArrayList<String> newArray = new ArrayList<String>();
+        newArray.addAll(groceryList.getGroceryList());
+
+        ArrayList<String> nextArray = new ArrayList<String>(groceryList.getGroceryList());
+
+        String[] myArray = new String[groceryList.getGroceryList().size()];
+        myArray = groceryList.getGroceryList().toArray(myArray);
+
+
+    }
+
+}
