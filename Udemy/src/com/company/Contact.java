5bed9d6fb3714f431479376c8d0c2fe4f1e48a7d
Qamas95 <kamil.p.kubiak@gmail.com>
2020-01-15 22:56:10 +0100

    Task: 		// Create a program that implements a simple mobile phone with the following capabilities.         // Able to store, modify, remove and query contact names.         // You will want to create a separate class for Contacts (name and phone number).         // Create a master class (MobilePhone) that holds the ArrayList of Contacts         // The MobilePhone class has the functionality listed above.         // Add a menu of options that are available.         // Options:  Quit, print list of contacts, add new contact, update existing contact, remove contact         // and search/find contact.         // When adding or updating be sure to check if the contact already exists (use name)         // Be sure not to expose the inner workings of the Arraylist to MobilePhone         // e.g. no ints, no .get(i) etc         // MobilePhone should do everything with Contact objects only.

--- Udemy/src/com/company/Contact.java
+++ Udemy/src/com/company/Contact.java
@@ -20,6 +20,6 @@ public class Contact {
     }
 
     public static Contact createContact(String name, String phoneNumber){
-        return new Contact(name,phoneNumber)
+        return new Contact(name,phoneNumber);
     }
 }
--- Udemy/src/com/company/MobilePhone.java
+++ Udemy/src/com/company/MobilePhone.java
@@ -25,20 +25,36 @@ public class MobilePhone {
         return this.myContacts.indexOf(contact);
     }
 
+    public boolean removeContact(Contact contact) {
+        int foundPosition = findContact(contact);
+        if (foundPosition < 0) {
+            System.out.println(contact.getName() + ", was not found");
+            return false;
+        }
+        this.myContacts.remove(foundPosition);
+        System.out.println(contact.getName() + ", was deleted.");
+        return true;
+    }
+
     private int findContact(String contactName) {
         for (int i = 0; i < myContacts.size(); i++) {
             Contact contact = this.myContacts.get(i);
-            if(contact.getName().equals(contactName)){
+            if (contact.getName().equals(contactName)) {
                 return i;
             }
         }
-        return  -1;
+        return -1;
     }
 
-    public boolean updateContact(Contact oldContact, Contact newContact){
+
+    public boolean updateContact(Contact oldContact, Contact newContact) {
         int foundPosition = findContact(oldContact);
-        if (foundPosition < 0){
+        if (foundPosition < 0) {
             System.out.println(oldContact.getName() + ", was not found.");
+            return false;
+        } else if (findContact(newContact.getName()) != -1){
+            System.out.println("Contact wiith name " + newContact.getName() +
+                    " already exists. Update was not successful.");
             return false;
         }
 
@@ -46,5 +62,29 @@ public class MobilePhone {
         System.out.println(oldContact.getName() + ", was replaced with " + newContact.getName());
         return true;
     }
+
+    public String queryContact(Contact contact) {
+        if (findContact(contact) >= 0) {
+            return contact.getName();
+        }
+        return null;
+    }
+
+    public Contact queryContact(String name) {
+        int position = findContact(name);
+        if (position >= 0) {
+            return this.myContacts.get(position);
+        }
+        return null;
+    }
+
+    public void printContacts() {
+        System.out.println("Contact list");
+        for (int i = 0; i < this.myContacts.size(); i++) {
+            System.out.println((i + 1) + "." + this.myContacts.get(i).getName() + "->" +
+                    this.myContacts.get(i).getPhoneNumber());
+        }
+    }
+
 
 }
