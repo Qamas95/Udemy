b85fc1b6576630fbed50ae09ecddcd3ee09e3d0a
Qamas95 <kamil.p.kubiak@gmail.com>
2020-01-21 22:29:03 +0100

    Task 1/3 		// You job is to create a simple banking application.         // There should be a Bank class         // It should have an arraylist of Branches         // Each Branch should have an arraylist of Customers         // The Customer class should have an arraylist of Doubles (transactions)         // Customer:         // Name, and the ArrayList of doubles.         // Branch:         // Need to be able to add a new customer and initial transaction amount.         // Also needs to add additional transactions for that customer/branch         // Bank:         // Add a new branch         // Add a customer to that branch with initial transaction         // Add a transaction for an existing customer for that branch         // Show a list of customers for a particular branch and optionally a list         // of their transactions         // Demonstration autoboxing and unboxing in your code         // Hint: Transactions         // Add data validation.         // e.g. check if exists, or does not exist, etc.         // Think about where you are adding the code to perform certain actions

--- Udemy/src/com/company/Main.java
+++ Udemy/src/com/company/Main.java
@@ -3,78 +3,12 @@ package com.company;
 
 import java.util.ArrayList;
 
-class IntClass{
-    private int myValue;
 
-    public IntClass(int myValue) {
-        this.myValue = myValue;
-    }
-
-    public int getMyValue() {
-        return myValue;
-    }
-
-    public void setMyValue(int myValue) {
-        this.myValue = myValue;
-    }
-
-}
-
-
-
-public class Main<dbl> {
+public class Main {
 
 
     public static void main(String[] args) {
 
-        String[] strArray = new String[10];
-        int[] intArray = new int[10];
-
-        ArrayList<String> stringArrayList = new ArrayList<String>();
-        stringArrayList.add("Qamas");
-
-        ArrayList<IntClass> intClassArrayList = new ArrayList<IntClass>();
-        intClassArrayList.add(new IntClass(54));
-
-        Integer integer = new Integer(54);
-        Double doubleValue = new Double(54.55);
-
-      /*  ArrayList<Integer> intArrayList = new ArrayList<Integer>();
-        for(int i=0; i<intArrayList.size(); i++){
-            intArrayList.add(Integer.valueOf(i));
-        }
-
-        for(int i=0; i<=10; i++){
-            System.out.println(i + " ---> " + intArrayList.get(i).intValue());
-
-        }
-*/
-        ArrayList<Integer> intArrayList = new ArrayList<Integer>();
-        for(int i=0; i<=10; i++) {
-            intArrayList.add(Integer.valueOf(i));
-        }
-
-        for(int i=0; i<=10; i++) {
-            System.out.println(i + " --> " + intArrayList.get(i).intValue());
-        }
-
-
-
-        Integer myIntValue = Integer.valueOf(56);
-        int myInt = myIntValue.intValue(); // myInt.intValue();
-
-        ArrayList<Double> myDoubleValues = new ArrayList<Double>();
-
-        for(double dbl=0.0; dbl<= 10.0; dbl += 0.5){
-        myDoubleValues.add(dbl);
-        }
-
-        for(int i = 0; i<myDoubleValues.size(); i++){
-            double value = myDoubleValues.get(i);
-            System.out.println(i + "  --->  " + value);
-        }
-
 
     }
-
 }
