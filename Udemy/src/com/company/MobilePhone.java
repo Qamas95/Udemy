57be5db55862cafa8b5ccb4886845b53b2ed3dfb
Qamas95 <kamil.p.kubiak@gmail.com>
2020-02-02 18:51:25 +0100

    Interfaces 2/2 - fixed

--- Udemy/src/com/company/MobilePhone.java
+++ Udemy/src/com/company/MobilePhone.java
@@ -1,89 +1,57 @@
-package com.company;
+package com.udemy;
 
-import java.util.ArrayList;
+public class MobilePhone implements ITelephone {
 
-public class MobilePhone {
+    private int myNumber;
+    private boolean isRinging;
+    private boolean isOn = false;
 
-    private String myNumber;
-    private ArrayList<Contact> myContacts;
 
-    public MobilePhone(String myNumber) {
+    public MobilePhone(int myNumber) {
         this.myNumber = myNumber;
-        this.myContacts = new ArrayList<Contact>();
     }
 
-    public boolean addNewContact(Contact contact) {
-        if (findContact(contact.getName()) >= 0) {
-            System.out.println("Contact is already on file ");
-            return false;
-        }
-        myContacts.add(contact);
-        return true;
+    @Override
+    public void powerOn() {
+        isOn = true;
+        System.out.println("Mobile phone powered up");
+       // System.out.println("No action taken, desk phone does not have power button");
     }
 
-    private int findContact(Contact contact) {
-        return this.myContacts.indexOf(contact);
-    }
-
-    public boolean removeContact(Contact contact) {
-        int foundPosition = findContact(contact);
-        if (foundPosition < 0) {
-            System.out.println(contact.getName() + ", was not found");
-            return false;
-        }
-        this.myContacts.remove(foundPosition);
-        System.out.println(contact.getName() + ", was deleted.");
-        return true;
-    }
-
-    private int findContact(String contactName) {
-        for (int i = 0; i < myContacts.size(); i++) {
-            Contact contact = this.myContacts.get(i);
-            if (contact.getName().equals(contactName)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-
-    public boolean updateContact(Contact oldContact, Contact newContact) {
-        int foundPosition = findContact(oldContact);
-        if (foundPosition < 0) {
-            System.out.println(oldContact.getName() + ", was not found.");
-            return false;
-        } else if (findContact(newContact.getName()) != -1){
-            System.out.println("Contact wiith name " + newContact.getName() +
-                    " already exists. Update was not successful.");
-            return false;
+    @Override
+    public void dial(int phoneNumber) {
+        if(isOn)
+        {
+            System.out.println("Now ringing " + phoneNumber + " on deskphone.");
+        } else {
+            System.out.println("Phone is switched off");
         }
 
-        this.myContacts.set(foundPosition, newContact);
-        System.out.println(oldContact.getName() + ", was replaced with " + newContact.getName());
-        return true;
     }
 
-    public String queryContact(Contact contact) {
-        if (findContact(contact) >= 0) {
-            return contact.getName();
+    @Override
+    public void answer() {
+        if (isRinging) {
+            System.out.println("Answering mobile phone");
+            isRinging = false;
         }
-        return null;
     }
 
-    public Contact queryContact(String name) {
-        int position = findContact(name);
-        if (position >= 0) {
-            return this.myContacts.get(position);
+    @Override
+    public boolean callPhone(int phoneNumber) {
+        if(phoneNumber == myNumber && isOn) {
+            System.out.println("Melody ring");
+            isRinging = true;
+        } else {
+            isRinging = false;
+            System.out.println("Mobile phone not on or number different.");
         }
-        return null;
+        return isRinging;
     }
 
-    public void printContacts() {
-        System.out.println("Contact list");
-        for (int i = 0; i < this.myContacts.size(); i++) {
-            System.out.println((i + 1) + "." + this.myContacts.get(i).getName() + "->" +
-                    this.myContacts.get(i).getPhoneNumber());
-        }
+    @Override
+    public boolean isRinging() {
+        return isRinging;
     }
 
 
