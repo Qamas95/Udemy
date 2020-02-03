c76296a2d554b0aca3530db2881fce261ae55a28
Qamas95 <kamil.p.kubiak@gmail.com>
2020-02-03 22:19:25 +0100

    Task 1/2 ---> with error // Create a simple interface (tip: name it Saveable or ISaveable) // That interface allows an object to be saved to some sort of storage medium. (tip: use arraylist) // The exact type of medium is not known to the interface (nor to the classes that implement it). // The interface will just specify two methods, one to return an ArrayList of values to be saved // and the other to populate the object's fields from an ArrayList (parameter). // // Create a few sample classes that implement your Saveable interface (we've used the idea of a game, // with Players and Monsters, but you can create any type of classes that you want). // // Override the toString() method for each of your classes so that they can be easily printed to enable // the program to be tested easier. // // In Main, write a method that takes an object that implements the interface as a parameter and // "saves" the values e.g. calls the method defined in the interface.
    // We haven't covered I/O yet, so your method should just print the values to the screen.
    // Also in the Main class, write a method that restores the values to a Saveable object
    // e.g. calls the method from the interface for populating fields (see above).
    
    // Again, we are not going to use Java file I/O; instead use the readValues() method below to
    // simulate getting values from a file – this allows you to type as many values as your class
    // requires, and returns an ArrayList.
    // There is a whole Java I/O section later in the course where you will get to use files, etc.
    
    public static ArrayList<String> readValues() {
            ArrayList<String> values = new ArrayList<String>();
    
            Scanner scanner = new Scanner(System.in);
            boolean quit = false;
            int index = 0;
            System.out.println("Choose\n" +
                    "1 to enter a string\n" +
                    "0 to quit");
    
            while (!quit) {
                System.out.print("Choose an option: ");
                int choice = scanner.nextInt();
                scanner.nextLine();
                switch (choice) {
                    case 0:
                        quit = true;
                        break;
                    case 1:
                        System.out.print("Enter a string: ");
                        String stringInput = scanner.nextLine();
                        values.add(index, stringInput);
                        index++;
                        break;
                }
            }
            return values;
        }

---
+++ Udemy/src/com/company/Player.java
@@ -0,0 +1,85 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player implements ISaveable {
+
+    private String name;
+    private int healthPoints;
+    private int strength;
+    private String weapon;
+
+    public Player(String name, int healthPoints, int strength) {
+        this.name = name;
+        this.healthPoints = healthPoints;
+        this.strength = strength;
+        this.weapon = "Axe";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", healthPoints=" + healthPoints +
+                ", strength=" + strength +
+                ", weapon='" + weapon + '\'' +
+                '}';
+    }
+
+    @Override
+    public List<String> write() {
+        List<String> values = new ArrayList<>();
+        values.add(0, this.name);
+        values.add(1 ,"" + this.healthPoints);
+        values.add(2,"" + this.strength);
+        values.add(3,"" + this.weapon);
+        return null;
+    }
+
+    @Override
+    public void read(List<String> savedValues) {
+        if(savedValues != null && savedValues.size() > 0) {
+            this.name = savedValues.get(0);
+
+           // this.healthPoints = savedValues.get(1); --> tak nie ruszy trzeba parseInt
+
+            this.healthPoints = Integer.parseInt(savedValues.get(1));
+            this.strength = Integer.parseInt(savedValues.get(2));
+            this.weapon = savedValues.get(3);
+        }
+    }
+}
