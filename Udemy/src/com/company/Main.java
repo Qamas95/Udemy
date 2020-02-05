515df0147ca909fabada16628880769343735b62
Qamas95 <kamil.p.kubiak@gmail.com>
2020-02-05 22:35:16 +0100

    Inner Classes 2/2

--- Udemy/src/com/company/Main.java
+++ Udemy/src/com/company/Main.java
@@ -6,42 +6,55 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btbPrint = new Button("Print");
+
 
     public static void main(String[] args) {
 
+/*        class ClickListener implements Button.OnClickListener {
+            public ClickListener() {
+                System.out.println("I've been attached");
+            }
 
-        Player qamas = new Player("Qamas",10,100);
-        System.out.println(qamas.toString());
-        saveObject(qamas);
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        }
 
-        qamas.setHealthPoints(8);
-        System.out.println(qamas);
-        qamas.setWeapon("War Axe");
-        saveObject(qamas);
-        //loadObject(qamas);
-        System.out.println(qamas);
+        btbPrint.setOnClickListener(new ClickListener());
+       */
+
+        btbPrint.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        });
+        listen();
+
+  /*      GearBox skoda = new GearBox(5);
+        skoda.operateClutch(true);
+        skoda.changeGear(1);
+        skoda.operateClutch(false);
+        System.out.println(skoda.wheelSpeed(1000));
+        skoda.changeGear(2);
+        System.out.println(skoda.wheelSpeed(1000));
+        skoda.operateClutch(true);
+        skoda.changeGear(3);
+        skoda.operateClutch(false);
+        System.out.println(skoda.wheelSpeed(6000));
 
 
-        System.out.println("=============");
-        ISaveable warewolf = new Monster("Warewolf",20,40);
-        System.out.println("Strength = " + ((Monster) warewolf).getStrength());
-        System.out.println(warewolf);
-        saveObject(warewolf);
 
+*/
     }
 
-    public static ArrayList<String> readValues() {
-        ArrayList<String> values = new ArrayList<String>();
 
-        Scanner scanner = new Scanner(System.in);
+    private static void listen() {
         boolean quit = false;
-        int index = 0;
-        System.out.println("Choose\n" +
-                "1 to enter a string\n" +
-                "0 to quit");
-
         while (!quit) {
-            System.out.print("Choose an option: ");
             int choice = scanner.nextInt();
             scanner.nextLine();
             switch (choice) {
@@ -49,26 +62,9 @@ public class Main {
                     quit = true;
                     break;
                 case 1:
-                    System.out.print("Enter a string: ");
-                    String stringInput = scanner.nextLine();
-                    values.add(index, stringInput);
-                    index++;
-                    break;
+                    btbPrint.onClick();
             }
         }
-        return values;
-    }
-
-
-    public static void saveObject(ISaveable objectToSave) {
-        for (int i = 0; i < objectToSave.write().size(); i++) {
-            System.out.println("Saving " + objectToSave.write().get(i) + " to storage device");
-        }
-    }
-
-    public static void loadObject(ISaveable objectToLoad){
-        ArrayList<String> values = readValues();
-        objectToLoad.read(values);
     }
 
 }
